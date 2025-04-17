a7a72045c2622a80085ce61177f147e15b023d74
curoky <cccuroky@gmail.com>
2025-04-18 00:16:03 +0800

    feat(playgroud): init

---
+++ playgroud/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+version=$1
+package_name=$2
+
+docker buildx build . \
+  --file Dockerfile.$version \
+  --network=host \
+  --build-arg PACKAGE_NAME=$package_name \
+  --tag curoky/dotbox:prebuilt-tools
